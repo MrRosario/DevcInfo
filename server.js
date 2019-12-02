035e9e71672b7b31254dce886b2930848d6c0714
MrRosario <eugenio.dorosario94@gmail.com>
2019-12-01 23:04:44 -0300

    make it better

--- server.js
+++ server.js
@@ -18,22 +18,26 @@ app.get('/', (req, res) => {
     res.render('pages/index',{
         pageTitle: "Home"
     });
-    
+
 });
 
 app.get('/network', async (req, res) => {
 
-    const Network = {
-        publicIpv4: await publicIp.v4(),
-        publicIpv6: await publicIp.v6(),
-        privateIpv4: await internalIp.v4(),
-        privateIpv6: await internalIp.v6(),
+    try{
+        const Network = {
+            publicIpv4: await publicIp.v4(),
+            publicIpv6: await publicIp.v6(),
+            privateIpv4: await internalIp.v4(),
+            privateIpv6: await internalIp.v6(),
+        }
+    
+        res.render('pages/network',{
+            NetworkData: Network,
+            pageTitle: "Network"
+        });
+    }catch (error) {
+        console.error(error);
     }
-
-    res.render('pages/network',{
-        NetworkData: Network,
-        pageTitle: "Network"
-    });
 });
 
 app.get('/location', async (req, res) => {
