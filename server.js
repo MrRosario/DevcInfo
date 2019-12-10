17eb6be60af48ab0c56d5fc2b2db7a3fb38d3ecd
MrRosario <eugenio.dorosario94@gmail.com>
2019-12-10 20:07:18 -0300

    teste

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/network', async (req, res) => {
             privateIpv4: await internalIp.v4(),
             privateIpv6: await internalIp.v6(),
         }
+        console.log(Network);
     
         res.render('pages/network',{
             NetworkData: Network,
@@ -42,8 +43,17 @@ app.get('/network', async (req, res) => {
 
 app.get('/location', async (req, res) => {
     try {
-        let ip = await publicIp.v4();
 
+        const Network = {
+            publicIpv4: await publicIp.v4(),
+            publicIpv6: await publicIp.v6(),
+            privateIpv4: await internalIp.v4(),
+            privateIpv6: await internalIp.v6(),
+        }
+        console.log(Network);
+        
+        let ip = await publicIp.v4();
+        
         const baseUrl =  `http://api.ipstack.com/${ip}?access_key=b8a3261cc4b4d85e9f509e776d3d5228`;
         const response = await axios.get(baseUrl);
 
